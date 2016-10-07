c2b918458335d9d05b26c68304f62df74a797b73
SherylManzoor <sherylmanzoor@yahoo.com>
2016-10-07 00:26:29 -0400

    Part Assembly: Part labeling function
    Part Assembly: Part labeling function

---
+++ assembly/code/labelColor.m
@@ -0,0 +1,24 @@
+function [partColored]=labelColor(part)
+% labelColor
+% Input: a binary, 2D array part  (0= empty, 1 = filled)
+% Output: a 2D array with each filled cell labeled 1 or 2 (0=empty, 1= red, 2 = blue)
+%
+% Authors: Sheryl Manzoor and Aaron T. Becker, Sep 27, 2016
+
+if nargin==0  
+    part = [...
+        0,1,0,1,0;
+        1,1,1,1,1;
+        0,1,0,1,0;
+        1,1,1,1,1;
+        0,1,0,1,0];
+end
+
+
+part_size=size(part); %Part Size
+A=mod(1:part_size(1,1),2); % Array for row of the part
+B=mod(1:part_size(1,2),2); % Array for coulumn of the part
+C=+bsxfun(@eq, B, A.'); % Matrix of zeros and ones
+partColored=(1+C).*part; % Final Part with labels
+
+end
