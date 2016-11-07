32095416bb26d9ed38ec81705c29289cfedd79ac
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-07 12:05:28 -0600

    Defines hopper in the factory

--- assembly/code/hopper.m
+++ assembly/code/hopper.m
@@ -1,7 +1,9 @@
 function [hopper] = hopper(tileColor, numCopies,cols)
-% TODO: comments, maybe add input cols,  then use the matlab commands
-% commented out below
 %This function defines hopper
+% Output:hopper is the array of tiles, boundary of the hopper and the free
+% space inside the hopper
+%Authors: Sheryl Manzoor and Aaron T. Becker, October 19, 2016
+
 
 obs = 3;
 
@@ -20,24 +22,6 @@ hopper(2:1+rows,2:1+cols) = tileColor; %fillhopper with components
 hopper(2:end,end-1) = 0; % build output shoot for hopper
 hopper(2,end-2) = 0; %connect output shoot to tiles
 hopper(end-1,2:cols-mod(numCopies,cols)+1) = 0;
-%  replace some tiles with 0s to match numCopies
-
-% rows = ceil(numCopies/4);
-% hopper = zeros(rows,4);
-% cnt_numCopies = 0;
-% for i=1:rows
-%    for j=1:4
-%        if cnt_numCopies < numCopies
-%             hopper(i,j)= tileColor; 
-%             cnt_numCopies = cnt_numCopies + 1;
-%        end
-%    end
-% end
-% 
-% hopper = vertcat(ones(1,4)*obs,zeros(1,4),hopper,ones(1,4)*obs);
-% hopper = horzcat(ones(size(hopper,1),1)*obs,hopper,ones(size(hopper,1),1)*obs,zeros(size(hopper,1),1)...
-%     ,ones(size(hopper,1),1)*obs);
-% hopper(1,7) = obs;
-% hopper(2,6) = 0;
+%  replace some tiles with 0s to match numCopies    
 end
 %%%%%%%%%%%%define a hopper%%%%%%%%%%%%%
