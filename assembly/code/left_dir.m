c78b5297ac27a0e9e6f9551f7281e72c1233125a
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-07 12:07:42 -0600

    Function for left direction

--- assembly/code/left_dir.m
+++ assembly/code/left_dir.m
@@ -1,17 +1,13 @@
 function [partXYupdated, factoryObstacleAdditionArray, align] = left_dir(hopper,partXY,  tileXY)
  
-%Note to self: still-to-do - tile that is to be added at the bottom
-%edge of the part from left is not yet catered in the code
+%Function for the tile added through left move
+%%Authors: Sheryl Manzoor and Aaron T. Becker, Oct 19, 2016
 
 obs = 3;
-ws = 6; %6 times the width of the part
-l_obs = 4; %left obstacle size = 4 times the width of the part
 if nargin<1
    
-   partXY = [2 1; 2 2; 2 3; 2 4; 2 5; ...
-        3 1; 3 2; 3 3; 3 4; 3 5; ...
-        4 1; 4 2; 4 3; 4 4; 4 5];
-   tileXY = [1 2];
+   partXY = [5 5; 5 4; 5 3; 5 2; 5 1];
+   tileXY = [5 6];
    hopper = [3 3 3 3 3 3 3 3; ...
             3 0 0 0 0 0 0 3; ...
             3 1 1 1 1 3 0 3; ...
@@ -21,39 +17,48 @@ if nargin<1
    
 end
 
+
+init_hop_width = size(hopper,1);
+% init_hop_length = size(hopper,2);
+
 part_length = abs(max(partXY(:,2) - min(partXY(:,2))))+1; %length of part
 part_width = abs(max(partXY(:,1)) - min(partXY(:,1)))+1; %width of part
 
-hopper = vertcat(hopper, zeros(part_width,size(hopper,2)));
-hopper(end-part_width+1:end,end-2) = obs; %obstacle to stop the right motion
+%%%%%%%%%%%Add White space to finalize the width of the hopper%%%%%
+height_obs = abs(max(partXY(:,1) - tileXY(:,1))); %row position of the tile w.r.t part
+add_width = (2*part_width)+3+1+height_obs;
+hopper = vertcat(hopper, zeros(add_width,size(hopper,2)));
+
+
+%%%%%%%%%%Add Main Obstacle%%%%%%%%%%%%%%%%%%
+height_obs = abs(max(partXY(:,1) - tileXY(:,1))); %row position of the tile w.r.t part
+hopper(end-height_obs-2+1:end,end-2:end) = obs; %obstacle added
+                        
+                        
+%%%%%%%%%%Add Right stop Obstacle%%%%%%%%%%%%%
+hopper(init_hop_width+1:init_hop_width+part_width,end-2) = obs; 
+                        %obstacle to stop the right motion
                         %Obstacle height is same as width of the part
 
-hopper = vertcat(hopper, zeros(part_width*ws,size(hopper,2))); %define white space 
-                        %beneath the hopper for motion of the part
-                        
-%%%%add zeros to the left of hopper                       
-if (3*part_length - size(hopper,2))>0
-   cols2add = ceil(3*part_length - size(hopper,2));
-   hopper = horzcat(zeros(size(hopper,1),cols2add),hopper); 
+%%%%add zeros to the left of hopper 
+if (2*part_length)+1 >= size(hopper,2)-3 
+    cols2add =  (2*part_length)+1  - (size(hopper,2)-3);
+    hopper = horzcat(zeros(size(hopper,1),cols2add),hopper);
 end
 
-hopper = vertcat(hopper, obs*ones(2,size(hopper,2))); %define the bottom 
+%%%%%%%%%%Add Bottom stop Obstacle%%%%%%%%%%%%%
+hopper(end-1:end, 1:end) = obs; %define the bottom 
                         %obstacle to stop the "downward" motion of the part
+                        
+%%%%%%%%%Add Left Obstacle%%%%%%%%%
+hopper(size(hopper,1)-part_width-1-2-height_obs:size(hopper,1)-2,1) = obs; 
+%obstacle added to stop the 'Left' motion of the part        
 
-hopper(size(hopper,1)-(l_obs*part_width)-1:size(hopper,1)-2,1) = obs; 
-%obstacle added to stop the 'Left' motion of the part, Obstacle height is
-%three times the width of the part
+hopper(size(hopper,1)-part_width-1-2-height_obs,1:part_length+1) = obs; 
+%obstacle added to stop the 'Upward' motion of the part
 
-hopper(size(hopper,1)-(l_obs*part_width)-1,1:part_length+1) = obs; 
-%obstacle added to stop the 'Upward' motion of the part, Obstacle height is
-%the same as the width of the part
-align = size(hopper,1)-(l_obs*part_width)-1;
+align=size(hopper,1)-part_width-1-2-height_obs;
 
-posy_obs = abs(max(partXY(:,1) - tileXY(:,1))); %row position of the obstacle
-
-hopper(end-posy_obs-2+1:end,end-1) = obs; %obstacle added
-                        %to stop the 'Right' motion of the part and add the
-                        %tile. Obstacle height is same as width of the part
 
 factoryObstacleAdditionArray = hopper;
 partXYupdated =0;
