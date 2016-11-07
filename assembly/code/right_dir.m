8287fac325a913b6833455c386efc2c670594bfe
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-07 12:09:49 -0600

    Function for right direction

--- assembly/code/right_dir.m
+++ assembly/code/right_dir.m
@@ -1,7 +1,7 @@
 function [partXYupdated, factoryObstacleAdditionArray, align] = right_dir(hopper,partXY,  tileXY)
  
-%Note to self: still-to-do - tile that is to be added at the right most
-%edge from top of the part is not yet catered in the code
+%Function for the tile added through right move
+%%Authors: Sheryl Manzoor and Aaron T. Becker, Oct 19, 2016
 
 obs = 3;
 if nargin<1
@@ -19,51 +19,58 @@ if nargin<1
    
 end
 
-part_length = abs(max(partXY(:,2) - min(partXY(:,2))))+1;
+init_hop_width = size(hopper,1);
+
+part_length = abs(max(partXY(:,2) - min(partXY(:,2))))+1; %length of part
 part_width = abs(max(partXY(:,1)) - min(partXY(:,1)))+1; %width of part
 
-hopper = vertcat(hopper, zeros(part_width,size(hopper,2)));
-hopper(end-part_width+1:end,end-2) = obs; %obstacle to stop the right motion
+%%%%%%%%%%%Add White space to finalize the width of the hopper%%%%%
+% height_obs = abs(max(partXY(:,1) - tileXY(:,1))); %row position of the tile w.r.t part
+add_width = (3*part_width)+3+1+3;
+hopper = vertcat(hopper, zeros(add_width,size(hopper,2)));
+                        
+                        
+%%%%%%%%%%Add Right stop Obstacle%%%%%%%%%%%%%
+hopper(init_hop_width+1:init_hop_width+part_width,end-2) = obs; 
+                        %obstacle to stop the right motion
                         %Obstacle height is same as width of the part
 
+%%%%add zeros to the left of hopper 
+if (2*part_length)+1 >= size(hopper,2)-6 
+    cols2add =  (2*part_length)+1  - (size(hopper,2)-6);
+    hopper = horzcat(zeros(size(hopper,1),cols2add),hopper);
+end
 
-hopper = vertcat(hopper, zeros(part_width*5,size(hopper,2))); %define white space 
-                        %beneath the hopper for motion of the part
-
-%%%%add zeros to the left of hopper                       
-if (2.5*part_length - size(hopper,2))>0
-   cols2add = ceil(2.5*part_length - size(hopper,2));
-   hopper = horzcat(zeros(size(hopper,1),cols2add),hopper); 
-end                        
+%%%%%%%%%%Add Bottom stop Obstacle for Tile%%%%%%%%%%%%%
+hopper(end-1:end, 1:end) = obs; %define the bottom 
+                        %obstacle to stop the "downward" motion of the tile
                         
-hopper = vertcat(hopper, obs*ones(2,size(hopper,2))); %define the bottom 
-                        %obstacle to stop the "downward" motion of tile
+%%%%%%%%%%Add Bottom stop Obstacle for Part%%%%%%%%%%%%%
+hopper(end-3, 3:end-2) = obs; %define the bottom 
+                        %obstacle to stop the "downward" motion of the part
+                       
+                        
 
-%%%%define bottom obstacle for the part%%%start%%%%
-hopper(end-3,3:end-2) = obs; %bottom row
-hopper(end-3-part_width:end-4,3) = obs; %vertical column to stop left motion
-%%%%%define bottom obstacle for the part%%%ends%%%%%
+                        
+%%%%%%%%%Add Left Obstacle for Part%%%%%%%%%
+hopper(end-3-part_width:end-4,3) = obs; %vertical column to stop left motion 
 
-hopper(end-5-(2*part_width),3:part_length+3) = obs; %stop upward motion of the part
-align = size(hopper,1)-5-(2*part_width);
-
-% hopper(end-3-(3*part_width),1:part_length) = obs; 
-% %obstacle added to stop the 'Upward' motion of the part, Obstacle height is
-% %the same as the width of the part
+%%%%%%%%%Stop Upward Motion of Part%%%%%%%%%
+hopper(size(hopper,1)-(2*part_width)-1-1-3,3:3+part_length) = obs; 
+%obstacle added to stop the 'Upward' motion of the part
 
 
+%%%%%%%%%Add Left Obstacle for Tile%%%%%%%%%
+hopper(size(hopper,1)-(2*part_width)-1-1-3:size(hopper,1)-2,1) = obs; 
+%obstacle added to stop the 'Left' motion of the tile        
+
+%%%%%%%%%Stop Upward motion of Tile%%%%%%%%%
 open1 = abs(min(partXY(:,1)) - tileXY(:,1)); 
-hopper(end-5-(2*part_width)+open1,2) = obs; %stop upward motion of the tile
-% align = size(hopper,1)-5-(2*part_width)+open1;
+hopper(size(hopper,1)-(2*part_width)-1-1-3+open1,2) = obs; %stop upward motion of the tile
 
 
-hopper(end-5-(2*part_width)+open1:end-2,1) = obs; 
-%obstacle added to stop the 'Left' motion of the tile, Obstacle height is
-%three times the width of the part
+align=size(hopper,1)-(2*part_width)-1-1-3;
 
-%hopper(end-3-(3*part_width),1:part_length) = obs; 
-%obstacle added to stop the 'Upward' motion of the tile, Obstacle height is
-%the same as the width of the tile
 
 factoryObstacleAdditionArray = hopper;
 partXYupdated =0;
