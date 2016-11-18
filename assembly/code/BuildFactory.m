a3c30246d928901511af9580adbebe6ac676bf40
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-18 06:19:31 -0600

    Assembly Update

--- assembly/code/BuildFactory.m
+++ assembly/code/BuildFactory.m
@@ -13,7 +13,9 @@ function [IsPossible, factoryLayoutArray]=BuildFactory(partXY,numCopies)
 
 
 if nargin <1
-    partXY = [5 3;4 3;4 2;3 2;2 2;1 2];
+    %partXY = [5 3;4 3;4 2;3 2;2 2;1 2];
+    partXY = [5 3;4 3;4 2];
+%     partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8];
     %partXY = [5 5;5 4;5 3;5 2;5 1;4 3]; %for down, down, down....
     %partXY = [5 1;5 2;5 3;5 4;5 5]; %for left, left, left....
     %partXY = [1 1;2 1;3 1;4 1;5 1]; %for up, up, up....
@@ -21,7 +23,7 @@ if nargin <1
     %partXY = [5 5;4 5;3 5;2 5;1 5]; %for test1
     %partXY = [5 5;5 4;5 3]; %for test2
     %partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10];
-    numCopies = 20;
+    numCopies = 11;
 end
 
 factoryLayoutArray = [];
@@ -39,8 +41,9 @@ partXYbuild = sequenceXY(1,:); % the part has first tiles
 for i = 2:size(sequenceXY,1)
     
     XYcoord = sequenceXY(i,:);
-    [partXYbuild, factoryObstacleAdditionArray, align_new] = factoryAddTile(partXYbuild,  XYcoord, dirs(i-1), partColoredArray(XYcoord(1,1),XYcoord(1,2)),numCopies,i); 
-%partXYbuild=part built before adding a tile
+    [partXYbuild, factoryObstacleAdditionArray, align_new, hopper_size] = factoryAddTile(partXYbuild,  XYcoord, dirs(i-1), partColoredArray(XYcoord(1,1),XYcoord(1,2)),numCopies,i); 
+    hopper_size = hopper_size-2;
+    %partXYbuild=part built before adding a tile
  %XYcoord = coordinates of the new tile to be added to the part
  %dirs(i-1)=direction of the new tile
  %partColoredArray(XYcoord)= color of new tile
@@ -52,20 +55,16 @@ for i = 2:size(sequenceXY,1)
     
     if i==2 %Builds the first factory's layout
         tileColor = partColoredArray(sequenceXY(1,1),sequenceXY(1,2));
-        hopper2 = hopper(tileColor, numCopies,4);
+        hopper2 = hopper_ml(tileColor, numCopies,4,1);        
         %%%%%%%%%%%%ends%%%%%%%%%%%%%%%%%%%%%%%%
         [~, factoryLayoutArray] = first_fac(hopper2);
         
     end
     
     if i==2
-        factoryLayoutArray = vertcat(zeros(4,size(factoryLayoutArray,2)),factoryLayoutArray);
-    else
         factoryLayoutArray = vertcat(zeros(2,size(factoryLayoutArray,2)),factoryLayoutArray);
     end
-    
-    factoryLayoutArray = concat_factories(factoryLayoutArray,factoryObstacleAdditionArray, align_prev,ceil(numCopies/4)+(((i-1)*4)-2));
-    hopper_size = ceil(numCopies/4)+(((i-1)*4)-2);
+    factoryLayoutArray = concat_factories(factoryLayoutArray,factoryObstacleAdditionArray, align_prev,hopper_size);
     if align_prev ==0
         num =0 ;
     else
