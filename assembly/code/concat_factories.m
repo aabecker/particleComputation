3759f94b3d7301a9f7c308b800ad0f1961482c9e
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-18 16:02:18 -0600

    Assembly Update: Concatenates factories

--- assembly/code/concat_factories.m
+++ assembly/code/concat_factories.m
@@ -1,25 +1,27 @@
 function [factoryLayoutArray] = concat_factories(all_prev, new, align, hopper_size)
 %Function concatenates all the factory layouts
 %Authors: Sheryl Manzoor and Aaron T. Becker, Oct 19, 2016
+        obs = 3;
         [rp, cp] = size(all_prev); %size of previous factories
         [~, cn] = size(new); %size of new factory
         
         if align ==0
             temp_new = new;
         else
-            num = align - (hopper_size+2)-1;  %rows to be added to new factory
-            temp_new = vertcat(zeros(num,cn),new);
+            num = align - (hopper_size+2)-1; %rows to be added to new factory
+            %temp_new = vertcat(zeros(num,cn),new);
+            temp_new = vertcat(obs*ones(num,cn),new);
         end
         
         [rn, ~] = size(temp_new);
         temp_prev = all_prev;
         if rn >= rp
-            temp_prev = vertcat(all_prev,zeros(rn-rp,cp));
+            %temp_prev = vertcat(all_prev,zeros(rn-rp,cp));
+            temp_prev = vertcat(all_prev,obs*ones(rn-rp,cp));
         else
-            temp_new = vertcat(temp_new,zeros(rp-rn,cn));
+            %temp_new = vertcat(temp_new,zeros(rp-rn,cn));
+            temp_new = vertcat(temp_new,obs*ones(rp-rn,cn));
         end
         
         factoryLayoutArray = horzcat(temp_prev,temp_new);
-
-
 end
