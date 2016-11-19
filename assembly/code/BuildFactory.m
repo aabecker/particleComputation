ea3200639d20d61cc0769d5ab13935a032af5f99
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-18 21:08:47 -0600

    Assembly Update: Builds Factory

--- assembly/code/BuildFactory.m
+++ assembly/code/BuildFactory.m
@@ -13,17 +13,16 @@ function [IsPossible, factoryLayoutArray]=BuildFactory(partXY,numCopies)
 
 
 if nargin <1
-   partXY = [5 3;4 3;4 2;3 2;2 2;1 2];
-%    partXY = [5 3;4 3;4 2];
-%     partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10];
-    %partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8];
-    %partXY = [5 5;5 4;5 3;5 2;5 1;4 3]; %for down, down, down....
+%    partXY = [5 3;4 3;4 2;3 2;2 2;1 2];
+    %partXY = [5 3;4 3;4 2;3 2];
+    partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8];
+%     partXY = [5 5;5 4;5 3;5 2;5 1;4 3]; %for down, down, down....
     %partXY = [5 1;5 2;5 3;5 4;5 5]; %for left, left, left....
     %partXY = [1 1;2 1;3 1;4 1;5 1]; %for up, up, up....
     %partXY = [5 5;5 4;5 3;5 2;5 1]; %for right, right, right....
     %partXY = [5 5;4 5;3 5;2 5;1 5]; %for test1
     %partXY = [5 5;5 4;5 3]; %for test2
-    %partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10];
+%     partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10];
     numCopies = 11;
     obs=3;
 end
@@ -53,12 +52,14 @@ for i = 2:size(sequenceXY,1)
  %Outputs: partXYbuild = part after addition of new tile
  %facoryObstacleAdditionArray = array of part hopper, obstacles and free
  %space
-
+ 
+    
     if i==2 %Builds the first factory's layout
         tileColor = partColoredArray(sequenceXY(1,1),sequenceXY(1,2));
         hopper2 = hopper_ml(tileColor, numCopies,4,1);        
         %%%%%%%%%%%%ends%%%%%%%%%%%%%%%%%%%%%%%%
         [~, factoryLayoutArray] = first_fac(hopper2);
+        
     end
     
     if i==2
