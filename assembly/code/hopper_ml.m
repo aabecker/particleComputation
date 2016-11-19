8a517da745427742d271ecc7310cb7b9a32907ae
SherylManzoor <sherylmanzoor@yahoo.com>
2016-11-18 21:11:56 -0600

    Assembly Update: defines Hopper

--- assembly/code/hopper_ml.m
+++ assembly/code/hopper_ml.m
@@ -18,8 +18,10 @@ hopper = obs*ones(rows+2,cols+4) ; % build boundary for hopper
 hopper(2:1+rows,2:1+cols) = tileColor; %fillhopper with components
 hopper(2:end,end-1) = 0; % build output shoot for hopper
 hopper(2,end-2) = 0; %connect output shoot to tiles
-hopper(end-1,2:cols-mod(numCopies,cols)+1) = 0;
-%  replace some tiles with 0s to match numCopies    
+if mod(numCopies,cols)~=0
+    hopper(end-1,2:cols-mod(numCopies,cols)+1) = 0;
+end
+    %  replace some tiles with 0s to match numCopies    
 
 
 if pos>1
