c48403574e754ff367ca7d756738b1a60c5d8a96
SherylManzoor <sherylmanzoor@yahoo.com>
2017-02-15 19:19:43 -0600

    Updated fig 6

--- assembly/code/BuildFactory.m
+++ assembly/code/BuildFactory.m
@@ -12,7 +12,7 @@ function [IsPossible, factoryLayoutArray]=BuildFactory(partXY,numCopies)
 %Authors: Sheryl Manzoor and Aaron T. Becker, Sep 27, 2016
 
 if nargin <1
-    partXY = [5 3;4 3;4 2;3 2;2 2;1 2;1 3];
+%     partXY = [5 3;4 3;4 2;3 2;2 2;1 2;1 3];
 %     partXY = [5 3;4 3;4 2;3 2;2 2;1 2;1 3];
 %     partXY = [5 3;4 3;4 2;3 2];
 %     partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10];
@@ -27,7 +27,7 @@ if nargin <1
 %     partXY=[7 6;9 6;6 7;7 7;8 7;9 7;10 7;7 8;9 8;6 9;7 9;8 9;9 9;10 9;7 10;9 10]; % # part
 % partXY = [1 2;2 2;3 2;4 3;5 3;4 2]; %contains the item positions for the zigzag Part 
 % partXY=[4 2;5 2;6 2;7 2; 8 2; 8 3; 8 4; 8 5; 8 6; 8 7; 7 7; 6 7; 5 7; 4 7; 4 3; 4 4;4 5];    %spiral
-%  partXY=[5 5;6 5;6 6;6 7;5 7;4 7;3 7;3 6;3 5;3 4;3 3;4 3;5 3;6 3;7 3;8 3;8 4;8 5;8 6;8 7;8 8];%spiral inner node
+ partXY=[5 5;6 5;6 6;6 7;5 7;4 7;3 7;3 6;3 5;3 4;3 3;4 3;5 3;6 3;7 3;8 3;8 4;8 5;8 6;8 7;8 8];%spiral inner node
 %For collaborators
   %partXY=[2 2;1 3;2 3;2 4];
 % partXY=[5 5;5 6;5 7;5 8];
